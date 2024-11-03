bdbc54823955ab79266efb2a81d979e6edabd16c
zkhan04 <khanzayd11@gmail.com>
2024-11-03 14:48:14 -0600

    comments

--- src/dropdown.js
+++ src/dropdown.js
@@ -1,12 +1,18 @@
 const createDropDownState = () => {
+    /**
+     * creates a state object. whenever the state is changed, a bunch of event listeners fire
+     */
     let visible = true;
     const listeners = []
 
+    // toggles the visible flag, and triggers each listener
     const toggleVisibility = () => {
         visible = !visible;
         listeners.forEach(listener => listener(visible));
     };
 
+    // whenever we call onVisibleChange(callback) somewhere, that callback
+    // gets added to the list of things that should occur when the dropdown is toggled.
     const onVisibleChange = (callback) => {
         listeners.push(callback);
     };
@@ -19,9 +25,11 @@ const createDropDownState = () => {
 }
 
 const createDropDownController = (state, dropDownHeader, optionList) => {
-    // when something in the optionList is clicked, replace the dropDownHeader (I think?)
+    // when the dropDownHeader is clicked, toggle the visibility
     dropDownHeader.addEventListener("click", () => state.toggleVisibility());
 
+    // when the visibility is toggled, we want to edit CSS classes of the optionList so that
+    // it can be hidden/revealed appropriately.
     state.onVisibleChange((visible) => {
         if (visible) {
             optionList.classList.remove("closed");
@@ -33,13 +41,19 @@ const createDropDownController = (state, dropDownHeader, optionList) => {
     });
 }
 
+
 const createDropDownOptionList = (options, onclick) => {
+    /**
+     * creates the list of options that a user can select from.
+     */
     const optionList = document.createElement("div");
     optionList.classList.add("option-list");
     options.forEach(option => {
         const optionElement = document.createElement("div");
         optionElement.classList.add("dropdown-option");
         optionElement.innerHTML = option;
+
+        // the event listener is defined externally
         optionElement.addEventListener("click", () => onclick(option));
         optionList.appendChild(optionElement);
     });
@@ -48,6 +62,9 @@ const createDropDownOptionList = (options, onclick) => {
 }
 
 const createDropDownHeader = (title) => {
+    /**
+     * kinda self explanatory lol
+     */
     const dropDownHeader = document.createElement("div");
     dropDownHeader.innerHTML = title;
     dropDownHeader.classList.add("dropdown-header");
@@ -55,25 +72,31 @@ const createDropDownHeader = (title) => {
 }
 
 const createDropDown = (title, options) => {
-    const dropDown = document.createElement("div");
+    /**
+     * puts everything together
+     */
 
     const state = createDropDownState();
 
+    const dropDown = document.createElement("div");
+    dropDown.classList.add("dropdown");
+
     const dropDownHeader = createDropDownHeader(title);
 
-    const onclick = (option) => {
+    // when an option is clicked, we wanna replace the header text with that option
+    // and toggle the visibility
+    const optionOnClick = (option) => {
         dropDownHeader.innerHTML = option;
         state.toggleVisibility();
     }
-
-    const optionList = createDropDownOptionList(options, onclick);
+    
+    const optionList = createDropDownOptionList(options, optionOnClick);
 
     createDropDownController(state, dropDownHeader, optionList);
 
     dropDown.appendChild(dropDownHeader);
     dropDown.appendChild(optionList);
-    dropDown.classList.add("dropdown");
-
+    
     state.toggleVisibility();
     
     return dropDown;
